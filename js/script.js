b963ef0bb4c6aa9cc2cce9dfde9014c80a80456a
Abdelhamid-bedair <abdelhamid.bedair@gmail.com>
2021-11-11 17:49:57 +0200

    add new panorama and change the fov

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const panoImage = document.querySelector(".pano-image");
-const hall = "images/hall.jpg";
+const hall = "images/211111.jpg";
 
 
 const panorama = new PANOLENS.ImagePanorama(hall);
 const viewer = new PANOLENS.Viewer({
     container: panoImage,
+    cameraFov: 75,
 
 });
 
 viewer.add(panorama);
 viewer.addViewIndicator();
 viewer.getControl().rotateLeft(4.75);
-viewer.getControl().rotateUp(0.1);
+viewer.getControl().rotateUp(0);
