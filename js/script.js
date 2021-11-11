92cb84528e5987315ea96ded3688463c10ffa747
Abdelhamid-bedair <abdelhamid.bedair@gmail.com>
2021-11-11 19:20:39 +0200

    delete loader

--- js/script.js
+++ js/script.js
@@ -6,16 +6,9 @@ const panorama = new PANOLENS.ImagePanorama(hall);
 const viewer = new PANOLENS.Viewer({
     container: panoImage,
     cameraFov: 75,
-
-});
-
-panorama.addEventListener('load', (event) => {
-    container.style.display = 'none';
 });
 
 viewer.add(panorama);
 viewer.addViewIndicator();
 viewer.getControl().rotateLeft(4.75);
 viewer.getControl().rotateUp(-0.05);
-
-const container = document.querySelector(".container");
